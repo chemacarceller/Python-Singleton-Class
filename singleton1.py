dbc1f9ce4d53b867cb967dc41f19ef98c0f8d3c5
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-03-26 19:24:48 +0100

    Add files via upload

--- singleton1.py
+++ singleton1.py
@@ -1,20 +1,34 @@
 # Singleton version 1
 # The class name should be changed; instead of "Singleton," the name of the class we want to behave like a singleton should be used.
+# If we need more than one singleton class, we must copy and paste this code.
 
-class Singleton:
+class Singleton :
+
+    # static variables (class variables in Python terminology)
     _instance = None
     _initialized = False
 
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls)
+    # Creation of the object
+    def __new__(cls, *args, **kwargs) :
+
+        # If _instance is None
+        if not cls._instance :
+
+            # The __new__ function of the object is called to allocate memory on the heap and create the object that we store in _instance.
+            cls._instance = super().__new__(cls)
+
+        # We return the object that was newly created or previously created.
         return cls._instance
 
-    def __init__(self, param1):
-        if self._initialized:
-            return 
-        self.attr1 = param1
-        self._initialized = True
+    # Object initialization
+    def __init__(self, param1) :
+
+        # We use a flag so that initialization only happens once.
+        if not self._initialized :
+
+            # Actual initialization
+            self.attr1 = param1
+            self._initialized = True
 
 # Testing
 s1 = Singleton("Value1") 
