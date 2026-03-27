ef34e8ab567af4bd43427c418733568377f4a00a
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-03-27 10:32:38 +0100

    Add files via upload

--- singleton3.py
+++ singleton3.py
@@ -1,17 +1,36 @@
 # Singleton version 3
+# This version uses a class decorator, a function that receives a class as a parameter that it decorates.
+# The decorator function returns the single instance of the object of the class it decorates, and this function is the one that controls it.
+# The decorator works as if its variables were static, not resetting between calls
 
+# decorator function
+def singleton(cls) :
 
-def singleton(cls):
+    # Dictionary that stores the unique instances of decorated classes
     instances = {}
-    def get_instance(*args, **kwargs):
-        if cls not in instances:
+
+    # method that applies the singleton pattern
+    def get_instance(*args, **kwargs) :
+
+        # if the instance does not exist
+        if cls not in instances :
+
+            # the instance is created
             instances[cls] = cls(*args, **kwargs)
+
+        # The function returns either a newly created instance or one that was previously stored.
         return instances[cls]
+    
+    # The decorator function returns either a newly created instance or a previously stored one.
     return get_instance
 
+# It is said that the MyClass class should be decorated by the decorator called singleton
 @singleton
 class MyClass :
-    def __init__(self, param1):
+
+    # Class initialization
+    def __init__(self, param1) :
+
         self.attr1 = param1
 
 # Testing
